8477f3c50ce39fe32268fc0b3b4cf3f9d78afdf3
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2023-09-16 13:56:54 +0100

    input handler added

---
+++ core/src/com/twofiftyfivebit/game/InputHandler.java
@@ -0,0 +1,25 @@
+package com.twofiftyfivebit.game;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector3;
+
+public class InputHandler extends InputAdapter
+{
+    private  OrthographicCamera camera;
+
+    InputHandler (OrthographicCamera camera)
+    {
+        this.camera = camera;
+
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button)
+    {
+        System.out.println(camera.unproject(new Vector3(screenX,screenY,0)));
+        return true;
+    }
+}
