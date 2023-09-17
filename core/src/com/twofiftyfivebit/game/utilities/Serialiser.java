bc752223cdac2fd050c163e7e43355dc3af9e95f
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2023-09-17 13:43:16 +0100

    level data loading added.

---
+++ core/src/com/twofiftyfivebit/game/utilities/Serialiser.java
@@ -0,0 +1,29 @@
+package com.twofiftyfivebit.game.utilities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Json;
+import com.twofiftyfivebit.game.data.LevelData;
+
+public class Serialiser
+{
+    private static Json json = new Json();
+
+    public static LevelData loadLevelData(AssetManager assetManager, int levelIndex)
+    {
+        String path = "levels/" + levelIndex + ".json";
+        FileHandle fileHandle = Gdx.files.internal(path);
+
+        LevelData levelData;
+
+        if (fileHandle.exists())
+        {
+            levelData = json.fromJson(LevelData.class, fileHandle);
+        } else
+        {
+            return null;
+        }
+        return levelData;
+    }
+}
