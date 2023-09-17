f063a21dd455a05a0d6c5f8d4bdb44001c35c3ba
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2023-09-17 13:42:28 +0100

    Implemented.

--- core/src/com/twofiftyfivebit/game/data/LevelData.java
+++ core/src/com/twofiftyfivebit/game/data/LevelData.java
@@ -4,4 +4,14 @@ public class LevelData
 {
     public  int width;
     public  int height;
+    public  Tile[] tiles;
+
+    public LevelData(){}
+
+    public LevelData(int width, int height, Tile[] tiles)
+    {
+        this.width = width;
+        this.height = height;
+        this.tiles = tiles;
+    }
 }
--- core/src/com/twofiftyfivebit/game/data/Tile.java
+++ core/src/com/twofiftyfivebit/game/data/Tile.java
@@ -1,5 +1,39 @@
 package com.twofiftyfivebit.game.data;
 
+import java.util.concurrent.TimeoutException;
+
 public class Tile
 {
+    public enum Type
+    {
+        straight,
+        bend,
+        junction,
+        start,
+        end
+    }
+
+    public Type type;
+    public int orientation;
+    public boolean[] connections;
+
+    public static Tile straight = new Tile(Type.straight,0,new boolean[]{true,false,true,false});
+
+    public Tile()
+    {
+    }
+
+    public Tile(Tile tile)
+    {
+        this.type = tile.type;
+        this.orientation = tile.orientation;
+        this.connections = tile.connections;
+    }
+
+    public Tile(Type type, int orientation, boolean[] connections)
+    {
+        this.type = type;
+        this.orientation = orientation;
+        this.connections = connections;
+    }
 }
