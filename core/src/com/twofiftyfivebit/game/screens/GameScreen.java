fec5c46215b55c8e23b9126cc68695bc56097b96
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2023-09-18 22:02:16 +0100

    GridRenderer added.

--- core/src/com/twofiftyfivebit/game/screens/GameScreen.java
+++ core/src/com/twofiftyfivebit/game/screens/GameScreen.java
@@ -6,26 +6,32 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.twofiftyfivebit.game.data.LevelData;
+import com.twofiftyfivebit.game.graphics.GridRenderer;
 import com.twofiftyfivebit.game.utilities.InputHandler;
+
+import java.util.ArrayList;
 
 public class GameScreen implements Screen
 {
     private OrthographicCamera camera;
     private FillViewport viewport;
-
+    final private float worldWidth = 16f;
+    final private float worldHeight = 9f;
     private SpriteBatch batch;
 
-    AssetManager assetManager;
-
+    private AssetManager assetManager;
     private InputHandler inputHandler;
-    private Texture background;
+    private GridRenderer gridRenderer;
 
-    final private float worldWidth = 32f;
-    final private float worldHeight = 18f;
+    private LevelData levelData;
+    private Texture[] tileTextures;
 
-    public GameScreen(AssetManager assetManager)
+    public GameScreen(AssetManager assetManager, LevelData levelData)
     {
         this.assetManager = assetManager;
 
@@ -34,10 +40,18 @@ public class GameScreen implements Screen
 
         batch = new SpriteBatch();
 
-        inputHandler = new InputHandler(camera);
+        this.levelData = levelData;
+
+        inputHandler = new InputHandler(camera,this.levelData);
         Gdx.input.setInputProcessor(inputHandler);
 
-        background = assetManager.get("Square.png", Texture.class);
+        tileTextures = new Texture[4];
+        tileTextures[0] = assetManager.get("0.png", Texture.class);
+        tileTextures[1] = assetManager.get("1.png", Texture.class);
+        tileTextures[2] = assetManager.get("2.png", Texture.class);
+        tileTextures[3] = assetManager.get("3.png", Texture.class);
+
+        gridRenderer = new GridRenderer(this.levelData,tileTextures);
     }
 
     @Override
@@ -49,7 +63,7 @@ public class GameScreen implements Screen
     @Override
     public void render(float delta)
     {
-        Gdx.gl.glClearColor(255, 0, 0, 1); // Set a clear color (e.g., black)
+        Gdx.gl.glClearColor(0.21f, 0.27f, 0.30f, 1.0f); // Set a clear color (e.g., black)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
@@ -57,8 +71,7 @@ public class GameScreen implements Screen
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
 
-        batch.draw(background, -8f, 0f, 1f, 1f);
-        batch.draw(background, 1f, 0f, 1f, 1f);
+        gridRenderer.render(batch);
 
         batch.end();
     }
@@ -91,5 +104,10 @@ public class GameScreen implements Screen
     public void dispose()
     {
         batch.dispose();
+
+        for (int i = 0; i < tileTextures.length; i++)
+        {
+            tileTextures[i].dispose();
+        }
     }
 }
