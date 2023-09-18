b69bf8c83b08fb01ede773fa4b627d85a8a34603
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2023-09-18 22:05:06 +0100

    Setters/Getters added.

--- core/src/com/twofiftyfivebit/game/data/LevelData.java
+++ core/src/com/twofiftyfivebit/game/data/LevelData.java
@@ -2,16 +2,50 @@ package com.twofiftyfivebit.game.data;
 
 public class LevelData
 {
-    public  int width;
-    public  int height;
-    public  Tile[] tiles;
+    public int width;
+    public int height;
+    public Tile[] tiles;
 
-    public LevelData(){}
+    public LevelData()
+    {
+    }
 
     public LevelData(int width, int height, Tile[] tiles)
     {
         this.width = width;
         this.height = height;
         this.tiles = tiles;
+    }
+
+    public Tile[] getTiles()
+    {
+        return tiles;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public int getTileCount()
+    {
+        return tiles.length;
+    }
+
+    public int getOrientation(int index)
+    {
+        return tiles[index].orientation;
+    }
+
+    public void onInput(int x, int y)
+    {
+        int index = x + y;
+        System.out.println(index);
+        tiles[index].rotate();
     }
 }
