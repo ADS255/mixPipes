f4b4f2a2da8e49fdb85fb35615676d1c1afe9a6a
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2023-09-19 22:08:45 +0100

    base functionality added.

--- core/src/com/twofiftyfivebit/game/utilities/InputHandler.java
+++ core/src/com/twofiftyfivebit/game/utilities/InputHandler.java
@@ -4,22 +4,71 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.twofiftyfivebit.game.data.LevelData;
 
 public class InputHandler extends InputAdapter
 {
-    private  OrthographicCamera camera;
+    private OrthographicCamera camera;
+    private LevelData levelData;
 
-    public InputHandler (OrthographicCamera camera)
+    private int width;
+    private int height;
+
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
+
+    private Vector2 gridOrigin;
+
+    public InputHandler(OrthographicCamera camera, LevelData levelData)
     {
         this.camera = camera;
+        this.levelData = levelData;
 
+        width = levelData.getWidth();
+        height = levelData.getHeight();
+
+        maxX = (width / 2 - 0.5f) * width;
+        minX = maxX * -1.0f;
+        maxY = (height / 2 - 0.5f) * height;
+        minY = maxY * -1.0f;
+
+        gridOrigin = new Vector2(width / 2*(-1.0f), height / 2*(-1.0f));
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button)
     {
-        System.out.println(camera.unproject(new Vector3(screenX,screenY,0)));
+        Vector3 screenCoordinates = new Vector3(screenX,screenY,0);
+        Vector3 touchPos = camera.unproject(screenCoordinates);
+
+        float x = touchPos.x;
+        float y = touchPos.y;
+
+        if (x >= minX && x <= maxX && y >= minY && y <= maxY)
+        {
+            int index = toGridIndex(x, y);
+            levelData.onInput(index);
+        }
+
         return true;
     }
+
+    private int toGridIndex(float xPos, float yPos)
+    {
+        int centerIndex = (levelData.getTileCount()-1)/2;
+        int clickIndex = centerIndex;
+        int clickX = Math.round(xPos);
+        int clickY = Math.round(yPos);
+
+        clickIndex += (height*clickX);
+        clickIndex += clickY;
+
+        return clickIndex;
+    }
+
 }
