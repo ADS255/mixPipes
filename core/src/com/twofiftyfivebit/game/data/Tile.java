0758dc943748148cc3e139776ac4e1e56583c910
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2023-09-25 14:59:41 +0100

    Texture lookup added for tile types.

--- core/src/com/twofiftyfivebit/game/data/Tile.java
+++ core/src/com/twofiftyfivebit/game/data/Tile.java
@@ -1,5 +1,7 @@
 package com.twofiftyfivebit.game.data;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 public class Tile
@@ -13,11 +15,22 @@ public class Tile
         end
     }
 
+    public static HashMap<Type, Integer> textureLookup = new HashMap<>();
+
+    static
+    {
+        textureLookup.put(Type.straight, 0);
+        textureLookup.put(Type.bend, 1);
+        textureLookup.put(Type.junction, 2);
+        textureLookup.put(Type.start, 3);
+        textureLookup.put(Type.end, 3);
+    }
+
     public Type type;
     public int orientation;
     public boolean[] connections;
 
-    public static Tile straight = new Tile(Type.straight,0,new boolean[]{true,false,true,false});
+    public static Tile straight = new Tile(Type.straight, 0, new boolean[]{true, false, true, false});
 
     public Tile()
     {
@@ -35,5 +48,15 @@ public class Tile
         this.type = type;
         this.orientation = orientation;
         this.connections = connections;
+    }
+
+    public static int getTextureIndex(Type tileType)
+    {
+        return textureLookup.get(tileType);
+    }
+
+    public void rotate()
+    {
+        orientation = (orientation + 1) % 4;
     }
 }
