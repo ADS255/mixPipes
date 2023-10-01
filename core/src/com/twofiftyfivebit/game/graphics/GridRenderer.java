3f1d91fcf3c873c934804bb70caa26830a47426d
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2023-10-01 11:19:02 +0100

    Multiple source grid traversal rendering added.

--- core/src/com/twofiftyfivebit/game/graphics/GridRenderer.java
+++ core/src/com/twofiftyfivebit/game/graphics/GridRenderer.java
@@ -5,31 +5,35 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.twofiftyfivebit.game.data.GameDataModel;
 import com.twofiftyfivebit.game.data.LevelData;
+import com.twofiftyfivebit.game.data.Tile;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GridRenderer
 {
-    LevelData levelData;
+    GameDataModel dataModel;
     Texture[] tileTextures;
     private Sprite[] sprites;
 
     private int width;
     private int height;
 
-    public GridRenderer(LevelData levelData, Texture[] tileTextures)
+    public GridRenderer(GameDataModel dataModel, Texture[] tileTextures)
     {
-        this.levelData = levelData;
+        this.dataModel = dataModel;
         this.tileTextures = tileTextures;
 
-        this.width = levelData.getWidth();
-        this.height = levelData.getHeight();
+        this.width = dataModel.getWidth();
+        this.height = dataModel.getHeight();
 
-        sprites = new Sprite[width*height];
+        sprites = new Sprite[width * height];
 
-        Vector2 offsetVec = new Vector2(width/2,height/2);
-        int index =0;
+        Vector2 offsetVec = new Vector2(width / 2, height / 2);
+        int index = 0;
 
         for (int i = 0; i < tileTextures.length; i++)
         {
@@ -40,18 +44,21 @@ public class GridRenderer
         {
             for (int y = 0; y < height; y++)
             {
-                float xPos = x-offsetVec.x-0.5f;
-                float yPos = y-offsetVec.y-0.5f;
-                float rotation = levelData.getOrientation(index)*90f;
+                Tile currentTile = dataModel.getTile(index);
+                float xPos = x - offsetVec.x - 0.5f;
+                float yPos = y - offsetVec.y - 0.5f;
+                float rotation = currentTile.orientation * 90f;
 
-                sprites[index] = new Sprite(tileTextures[0],0,0,tileTextures[0].getWidth(),tileTextures[0].getHeight());
+                int textureIndex = Tile.getTextureIndex(currentTile.type);
+                Texture spriteTexture = tileTextures[textureIndex];
+
+                sprites[index] = new Sprite(spriteTexture, 0, 0, tileTextures[0].getWidth(), tileTextures[0].getHeight());
                 Sprite sprite = sprites[index];
 
-                sprite.setPosition(xPos,yPos);
-                sprite.setSize(1f,1f);
+                sprite.setPosition(xPos, yPos);
+                sprite.setSize(1f, 1f);
                 sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
                 sprite.rotate(rotation);
-                System.out.println(sprite.getRotation());
                 sprite.setColor(Color.WHITE);
 
                 index++;
@@ -61,8 +68,39 @@ public class GridRenderer
 
     public void render(SpriteBatch batch)
     {
-        for (int i = 0; i < levelData.getTileCount(); i++)
+        for (int i = 0; i < dataModel.getTileCount(); i++)
         {
+            Tile tile = dataModel.getTile(i);
+            sprites[i].setRotation(tile.orientation * (-90f));
+
+            sprites[i].draw(batch);
+        }
+
+        int[] traversalData = dataModel.getVisited();
+
+        for (int i = 0; i < traversalData.length; i++)
+        {
+            int tileState = traversalData[i];
+
+            switch (tileState)
+            {
+                case 0:
+                    sprites[i].setColor(Color.WHITE);
+                    break;
+
+                case 1:
+                    sprites[i].setColor(Color.BLUE);
+                    break;
+
+                case 2:
+                    sprites[i].setColor(Color.YELLOW);
+                    break;
+
+                case 3:
+                    sprites[i].setColor(Color.GREEN);
+                    break;
+            }
+
             sprites[i].draw(batch);
         }
     }
